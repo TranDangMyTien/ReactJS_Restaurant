a7310a53b10bf2a75935d41304ef2fb692130aad
TranDangMyTien <mytien.2682003@gmail.com>
2025-02-23 14:48:30 +0700

    Responsive header

--- restaurant/src/components/Navbar/Navbar.js
+++ restaurant/src/components/Navbar/Navbar.js
@@ -3,10 +3,23 @@ import styled from "styled-components";
 import foodYummy from "../../assets/Epsilon.png";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { VscChromeClose } from "react-icons/vsc";
+
+
 export default function Navbar() {
   const [navbarState, setNavbarState] = useState(false);
-  const html = document.querySelector("html");
-  html.addEventListener("click", () => setNavbarState(false));
+
+
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (!e.target.closest(".responsive-nav") && !e.target.closest(".toggle")) {
+        setNavbarState(false);
+      }
+    };
+    document.addEventListener("click", handleOutsideClick);
+    return () => document.removeEventListener("click", handleOutsideClick);
+  }, []);
+
+
   return (
     <>
       <Nav>
@@ -16,100 +29,69 @@ export default function Navbar() {
             {navbarState ? (
               <VscChromeClose onClick={() => setNavbarState(false)} />
             ) : (
-              <GiHamburgerMenu
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setNavbarState(true);
-                }}
-              />
+              <GiHamburgerMenu onClick={() => setNavbarState(true)} />
             )}
           </div>
         </div>
         <ul className="links">
-          <li>
-            <a href="#home" className="active">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#services">Our Services</a>
-          </li>
-          <li>
-            <a href="#portfolio">Portfolio</a>
-          </li>
-          <li>
-            <a href="#testimonials">Testimonials</a>
-          </li>
-          <li>
-            <a href="#products">Products</a>
-          </li>
-          <li>
-            <a href="#newsletter">Newsletter</a>
-          </li>
+          <li><a href="/" className="active">Home</a></li>
+          <li><a href="#">Menu</a></li>
+          <li><a href="#p">Contact</a></li>
+          <li><a href="#products">Products</a></li>
+          <li><a href="#newsletter">Newsletter</a></li>
+          <li><button className="reserve-button">Reserve</button></li>
         </ul>
       </Nav>
-      <ResponsiveNav state={navbarState} className={navbarState ? "show" : ""}>
+
+      <ResponsiveNav state={navbarState} className="responsive-nav">
         <ul>
-          <li>
-            <a
-              href="#home"
-              className="active"
-              onClick={() => setNavbarState(false)}
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#services" onClick={() => setNavbarState(false)}>
-              Our Services
-            </a>
-          </li>
-          <li>
-            <a href="#portfolio" onClick={() => setNavbarState(false)}>
-              Portfolio
-            </a>
-          </li>
-          <li>
-            <a href="#testimonials" onClick={() => setNavbarState(false)}>
-              Testimonials
-            </a>
-          </li>
-          <li>
-            <a href="#products" onClick={() => setNavbarState(false)}>
-              Products
-            </a>
-          </li>
-          <li>
-            <a href="#newsletter" onClick={() => setNavbarState(false)}>
-              Newsletter
-            </a>
-          </li>
+          <li><a href="/">Home</a></li>
+          <li><a href="#">Menu</a></li>
+          <li><a href="#p">Contact</a></li>
+          <li><a href="#products">Products</a></li>
+          <li><a href="#newsletter">Newsletter</a></li>
+          <li><button className="reserve-button">Reserve</button></li>
         </ul>
       </ResponsiveNav>
     </>
   );
 }
 
+
 const Nav = styled.nav`
- background-color: black;
+  background-color: black;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 4vw;
+
+
   .brand {
+    display: flex;
+    align-items: center;
     img {
       margin-top: 1rem;
       cursor: pointer;
     }
     .toggle {
       display: none;
+      cursor: pointer;
+      font-size: 1.5rem;
+      color: white;
     }
   }
+
+
   .links {
     display: flex;
-    list-style-type: none;
+    list-style: none;
     gap: 2rem;
+   
     li {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+     
       a {
         color: #fc4958;
         font-weight: 600;
@@ -117,6 +99,12 @@ const Nav = styled.nav`
         text-transform: uppercase;
         letter-spacing: 0.2rem;
         transition: 0.3s ease-in-out;
+        padding: 10px 15px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+       
         &:hover {
           color: #f9c74f;
         }
@@ -125,14 +113,35 @@ const Nav = styled.nav`
         color: #f9c74f;
       }
     }
+
+
+    .reserve-button {
+      background-color: #fc4958;
+      color: black;
+      font-weight: bold;
+      font-size: 1.2rem;
+      padding: 0.8rem 1.5rem;
+      border: none;
+      border-radius: 10px;
+      cursor: pointer;
+      transition: 0.3s ease-in-out;
+     
+      &:hover {
+        background-color: #f9c74f;
+      }
+    }
   }
-  @media screen and (min-width: 260px) and (max-width: 1080px) {
+     @media screen and (max-width: 1300px) {
+     .links {
+   
+       gap: 10px;}
+     }
+
+
+  @media screen and (max-width: 1080px) {
     .brand {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
       width: 100%;
-      top: 0;
+      justify-content: space-between;
       .toggle {
         display: block;
       }
@@ -142,41 +151,73 @@ const Nav = styled.nav`
     }
   }
 `;
+
+
 const ResponsiveNav = styled.div`
   position: fixed;
-  right: -100vw;
+  right: ${({ state }) => (state ? "0" : "-100vw")};
   top: 0;
   z-index: 10;
   background-color: white;
   height: 100vh;
-  width: ${({ state }) => (state ? "60%" : "0%")};
+  width: 60%;
   transition: 0.3s ease-in-out;
   display: flex;
-  opacity: 0;
-  visibility: hidden;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 3rem;
+
+
   ul {
-    list-style-type: none;
+    list-style: none;
     width: 100%;
-    margin-top: 3rem;
+   
     li {
-      width: 100%;
-      margin: 1rem 0;
-      margin-left: 2rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 50px;
+      margin-bottom: 1rem;
+
+
       a {
         text-decoration: none;
         color: #f9c74f;
         font-size: 1.2rem;
         transition: 0.1s ease-in-out;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+       
         &:hover {
           color: #fc4958;
         }
       }
-      &:first-of-type {
-        a {
-          color: #fc4958;
-          font-weight: 900;
-        }
-      }
+    }
+  }
+
+
+  .reserve-button {
+    background-color: #fc4958;
+    color: black;
+    font-weight: bold;
+    font-size: 1.2rem;
+    padding: 0.8rem 1.5rem;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    margin-top: 2rem;
+    transition: 0.3s ease-in-out;
+
+
+    &:hover {
+      background-color: #f9c74f;
     }
   }
 `;
+
+
+
