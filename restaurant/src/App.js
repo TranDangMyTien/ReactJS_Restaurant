743453e7d73f00ee17b55d1cf74a781736f9674b
TranDangMyTien <106966807+TranDangMyTien@users.noreply.github.com>
2025-02-27 22:41:29 +0700

    Test function

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -6,20 +6,40 @@ import Home from "./components/Home/Home";
 import ScrollToTop from "./components/Home/ScrollToTop";
 import MenuPage from "./components/Menu/MenuPage";
 import Contact from "./components/Contact/Contact";
+import { useState, useEffect } from "react";
+import FloatingContactButtons from "./components/Contact/FloatingContactButtons";
 
 function App() {
+  const [showContactIcons, setShowContactIcons] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      // Hiển thị icon khi người dùng cuộn xuống khoảng 200px
+      if (window.scrollY > 200) {
+        setShowContactIcons(true);
+      } else {
+        setShowContactIcons(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup event listener khi component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <BrowserRouter>
       <div className="App">
+        <ScrollToTop />
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/menu" element={<MenuPage />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        <ScrollToTop />
         <Footer />
+        <FloatingContactButtons visible={showContactIcons} />
       </div>
     </BrowserRouter>
   );
